0773ca0f710b5950b4bd8901502fa561cdc3cfd3
ivanbaug <ivsitioinfo@gmail.com>
2022-01-04 18:43:21 -0500

    remove hardcoded default key

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("", views.get_users, name="users"),
     path("login/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("profile/", views.get_user_profile, name="users-profile"),
+    path("profile/", views.get_user_profile, name="user-profile"),
+    path("profile/update/", views.update_user_profile, name="user-profile-update"),
     path("register/", views.register_user, name="register"),
 ]
