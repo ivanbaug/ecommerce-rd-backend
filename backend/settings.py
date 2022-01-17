26ce67f32c209510027cef6aefc51e0dd41211e1
ivanbaug <ivsitioinfo@gmail.com>
2022-01-16 23:28:42 -0500

    add product reviews and pagination

--- backend/settings.py
+++ backend/settings.py
@@ -115,9 +115,17 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DBNAME"),
+        "USER": os.environ.get("DBUSER"),
+        "PASSWORD": os.environ.get("DBPASS"),
+        "HOST": os.environ.get("DBHOST"),
+        "PORT": os.environ.get("DBPORT"),
     }
 }
 
--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path("", views.get_products, name="products"),
     path("create/", views.create_product, name="product-create"),
     path("upload/", views.upload_image, name="image-upload"),
+    path("top/", views.get_top_products, name="top-products"),
     path("<str:pk>/reviews/", views.create_product_review, name="create-review"),
-    path("<str:pk>", views.get_product, name="product"),
+    path("<str:pk>/", views.get_product, name="product"),
     path("update/<str:pk>/", views.update_product, name="product-update"),
     path("delete/<str:pk>/", views.delete_product, name="product-delete"),
 ]
