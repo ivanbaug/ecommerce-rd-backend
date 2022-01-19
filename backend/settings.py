ec8b832bbe6c3a296a8ed15794af2941481c6604
ivanbaug <ivsitioinfo@gmail.com>
2022-01-18 21:23:41 -0500

    update app url for prod host

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", os.environ.get("GCP_URL")]
 
 
 # Application definition
